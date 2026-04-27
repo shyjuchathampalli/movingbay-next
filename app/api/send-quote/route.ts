6363ff474544cfb92d40ef6da4991a33488fd4b4
shyju-silcon <shyju@silcontechlabs.com>
2026-04-27 13:24:34 +0530

    movingbay credentials reset

--- app/api/send-quote/route.ts
+++ app/api/send-quote/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
 
     await resend.emails.send({
       from: "MovingBay <onboarding@resend.dev>",
-      to: ["info@movingbay.com,shyjuchathampalli@gmail.com"], // ✅ CHANGE THIS
+      to: ["info@movingbay.com"], // ✅ CHANGE THIS
       subject: "New Quote Request 🚀",
       html: `
         <div style="font-family: Arial, sans-serif; padding: 20px;">
